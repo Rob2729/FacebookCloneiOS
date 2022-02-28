1f262c0622d47f1698b89ed0a254199505e1f1d8
Rob2729 <Rob.niv@hotmail.com>
2022-02-28 18:22:05 +0000

    Initial Commit

---
+++ FacebookClone/FacebookCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookCloneApp.swift
+//  FacebookClone
+//
+//  Created by Robert on 28/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct FacebookCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
